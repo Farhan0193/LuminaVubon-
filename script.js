ee2ef4081e2df93f9e33b0c4ed19a129ef2dd419
Farhan <faarhanjunior76@gmail.com>
2026-07-27 14:18:58 +0600

    Implement scroll-based progress bar functionality
    Add scroll event listener to update progress bar width

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function toggleMenu() {
     const navbar = document.getElementById("navbar");
     navbar.classList.toggle("active");
 }
+window.addEventListener("scroll", function () {
+
+const winScroll = document.documentElement.scrollTop;
+
+const height =
+document.documentElement.scrollHeight -
+document.documentElement.clientHeight;
+
+const scrolled = (winScroll / height) * 100;
+
+document.getElementById("progress-bar").style.width =
+scrolled + "%";
+
+});
