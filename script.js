5e1d71d53138f3c9905137f3557b15d982611a10
Farhan <faarhanjunior76@gmail.com>
2026-09-20 22:57:51 +0600

    Remove Google Ads script from script.js
    Remove Google Ads script from the end of the file.

--- script.js
+++ script.js
@@ -182,4 +182,6 @@ alert("Thank you for subscribing to LuminaVubon🌐");
 });
 
 
-}
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2680089287126244"
+     crossorigin="anonymous"></script>
+   }
